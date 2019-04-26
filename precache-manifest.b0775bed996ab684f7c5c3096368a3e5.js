73d10d2e34efb3516192152ce13f0f136669b4c9
Abhishek <abhishek.aapte@gmail.com>
2019-04-26 11:42:26 +0530

    Updates

---
+++ precache-manifest.b0775bed996ab684f7c5c3096368a3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9330c14216cb8625112795d7eadaf2b6",
+    "url": "/crypto-app/index.html"
+  },
+  {
+    "revision": "b05742251f37ed83804b",
+    "url": "/crypto-app/static/css/main.defad1ec.chunk.css"
+  },
+  {
+    "revision": "7c255a69b751224cecf0",
+    "url": "/crypto-app/static/js/2.ab2d17df.chunk.js"
+  },
+  {
+    "revision": "b05742251f37ed83804b",
+    "url": "/crypto-app/static/js/main.221b005f.chunk.js"
+  },
+  {
+    "revision": "fbdc30d0ca24fff84313",
+    "url": "/crypto-app/static/js/runtime~main.ae00ff49.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/crypto-app/static/media/logo.5c0436d5.png"
+  }
+]);
