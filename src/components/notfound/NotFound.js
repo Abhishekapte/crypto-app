ec33c70eff7871bc1a091d0678be6ef7d8a78b8a
Abhishek <abhishek.aapte@gmail.com>
2019-04-27 15:46:41 +0530

    changed component
    update version

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -5,7 +5,7 @@ import './NotFound.css';
 const NotFound = () => {
   return (
     <div className="NotFound">
-      <h1 className="NotFound-title">Oops! Page not found</h1>
+      <h1 className="NotFound-title">Welcome to the Cryptocurrency price Index</h1>
       <Link to="/" className="NotFound-link">Go to home page</Link>
     </div>
   );
